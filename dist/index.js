9cf73a2193cb5a8d20c2382bcb4b0b65032bc145
JD Fortune <jondfortune@gmail.com>
2024-12-11 15:09:01 -0600

    updated file

--- dist/index.js
+++ dist/index.js
@@ -4,12 +4,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const fs = require('fs');
 const app = (0, express_1.default)();
 const PORT = 3000;
 const path = require('path');
-app.use('/images', express_1.default.static(path.join(__dirname, 'images')));
+app.use('/images', express_1.default.static(path.join(__dirname, 'public/images')));
 app.get('/', (req, res) => {
-    res.send('<img src="../../images/me.jpg" width="600" alt="picture of me">');
+    fs.readFile(__dirname + '/index.html', 'utf-8', (err, data) => {
+        if (err) {
+            console.error('Error reading HTML file:', err);
+            res.status(500).send('<h1>Internal Server Error</h1>');
+        }
+        else {
+            res.send(data);
+        }
+    });
+    // res.send('<img src="../../images/me.jpg" width="600" alt="picture of me">');
 });
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}.`);
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
 import express from 'express';
+const fs = require('fs');
 
 const app = express();
 const PORT = 3000;
 const path = require('path');
 
 
-app.use('/images', express.static(path.join(__dirname, 'images')))
+app.use('/images', express.static(path.join(__dirname, 'public/images')))
 
 app.get('/', (req, res) => {
-  res.send('<img src="../../images/me.jpg" width="600" alt="picture of me">');
+  fs.readFile(__dirname + '/index.html', 'utf-8', (err: unknown, data: string) => {
+    if (err) {
+      console.error('Error reading HTML file:', err);
+      res.status(500).send('<h1>Internal Server Error</h1>');
+    } else {
+      res.send(data);
+    }
+  })
+  // res.send('<img src="../../images/me.jpg" width="600" alt="picture of me">');
 });
 
 app.listen(PORT, () => {
