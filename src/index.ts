83731058690bef1df5333c01e8a6aeba20440eb0
JD Fortune <jondfortune@gmail.com>
2024-12-11 11:17:46 -0600

    hello

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const path = require('path');
 app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.get('/', (req, res) => {
-  res.send('<img src="../images/me.jpg" alt="picture of me">' + '<p>' + __dirname + 'images' + '</p>');
+  res.send('<img src="../images/me.jpg" width="600" alt="picture of me">' + '<p>' + __dirname + 'images' + '</p>');
 });
 
 app.listen(PORT, () => {
