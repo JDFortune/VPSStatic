97b1c2a16604d697deee29d48cc151cd39a12f25
JD Fortune <jondfortune@gmail.com>
2024-12-11 11:17:11 -0600

    hello

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,13 @@ import express from 'express';
 
 const app = express();
 const PORT = 3000;
+const path = require('path');
+
+
+app.use('/images', express.static(path.join(__dirname, 'images')))
 
 app.get('/', (req, res) => {
-  res.send('Request successfully sent to express app.');
+  res.send('<img src="../images/me.jpg" alt="picture of me">' + '<p>' + __dirname + 'images' + '</p>');
 });
 
 app.listen(PORT, () => {
